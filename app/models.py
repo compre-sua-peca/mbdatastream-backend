b17dcba2c0069d675dedfac6856a01335e44cbee
devops8-alvaro <devops8@compresuapeca.com.br>
2025-09-30 16:18:08 -0300

    fix: models tables and auth_routes

--- app/models.py
+++ app/models.py
@@ -1,17 +1,17 @@
 from .extensions import db
-
-
+ 
+ 
 class Category(db.Model):
     hash_category = db.Column(db.String(255), primary_key=True)
     name_category = db.Column(db.String(255), nullable=False)
     display_order = db.Column(db.Integer, default=0, nullable=False)
-
+ 
     products = db.relationship('Product', backref='category', lazy=True)
-
+ 
     def __repr__(self):
         return f"Category('{self.hash_category}', '{self.name_category}')"
-
-
+ 
+ 
 class Product(db.Model):
     cod_product = db.Column(db.String(255), primary_key=True)
     name_product = db.Column(db.String(255), nullable=False)
@@ -32,30 +32,30 @@ class Product(db.Model):
     id_manufacturer = db.Column(db.Integer, db.ForeignKey(
       'manufacturer.id', onupdate="CASCADE", ondelete="CASCADE"
     ), nullable=True)
-
+ 
     images = db.relationship('Images', backref='product', lazy=True, cascade="all" )
     compatibilities = db.relationship(
         'Compatibility', backref='product', lazy=True)
     manufacturers = db.relationship(
         'Manufacturer', backref='product', lazy=True)
-
-
+ 
+ 
     def __repr__(self):
         return f"Product('{self.cod_product}', '{self.name_product}', '{self.bar_code}', '{self.gear_quantity}', '{self.gear_dimensions}', '{self.cross_reference}', '{self.hash_category}')"
-
+ 
 class Manufacturer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
     description = db.Column(db.String(200), nullable=True)
     order = db.Column(db.Integer, nullable=False)
-
+ 
 class SellerManufacturer(db.Model):
     id_seller = db.Column(db.Integer, primary_key=True)
     id_manufacturer = db.Column(db.Integer, primary_key=True)
-
+ 
 class Images(db.Model):
     __tablename__ = "images"
-
+ 
     cod_product = db.Column(
         db.String(255),
         db.ForeignKey(
@@ -64,19 +64,19 @@ class Images(db.Model):
         primary_key=True,  # passa a fazer parte da PK composta
         nullable=False
     )
-
+ 
     id_image = db.Column(
         db.String(255),
         primary_key=True,  # continua como parte da PK composta
         nullable=False
     )
-
+ 
     url = db.Column(db.String(255), nullable=False)
-
+ 
     def __repr__(self):
         return f"Images('{self.cod_product}', '{self.id_image}', '{self.url}')"
-
-
+ 
+ 
 class Vehicle(db.Model):
     vehicle_name = db.Column(db.String(255), primary_key=True)
     start_year = db.Column(db.String(4), nullable=True)
@@ -85,13 +85,13 @@ class Vehicle(db.Model):
     hash_brand = db.Column(db.String(255), db.ForeignKey(
         'vehicle_brand.hash_brand', onupdate="CASCADE", ondelete="CASCADE"
     ), nullable=False)
-
+ 
     compatibilities = db.relationship(
         'Compatibility', backref='vehicle', lazy=True)
     vehicle_brand = db.relationship(
         'VehicleBrand', backref='vehicles', lazy=True)
-
-
+ 
+ 
 class Compatibility(db.Model):
     cod_product = db.Column(db.String(255), db.ForeignKey(
         'product.cod_product', onupdate="CASCADE", ondelete="CASCADE"
@@ -99,19 +99,19 @@ class Compatibility(db.Model):
     vehicle_name = db.Column(db.String(255), db.ForeignKey(
         'vehicle.vehicle_name', onupdate="CASCADE", ondelete="CASCADE"
     ), primary_key=True)
-
+ 
     def __repr__(self):
         return f"Compatibility('{self.cod_product}', '{self.vehicle_name}')"
-
-
+ 
+ 
 class VehicleBrand(db.Model):
     __tablename__ = "vehicle_brand"
     hash_brand = db.Column(db.String(255), primary_key=True)
     brand_name = db.Column(db.String(255), nullable=False)
     brand_image = db.Column(db.String(255), nullable=True)
     display_order = db.Column(db.Integer, default=0, nullable=False)
-
-
+ 
+ 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False)
@@ -119,29 +119,27 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     cnpj = db.Column(db.String(20), nullable=False)
     whatsapp = db.Column(db.String(150), nullable=False)
-
+ 
     def serialize(self):
         return {
             "username": self.username,
             "email": self.email
         }
-
-class SellerUsers(db.Model):
-    __tablename__ = 'seller_users'
+ 
+class User_seller(db.Model):
+    __tablename__ = 'user_seller'
     id_seller = db.Column(db.Integer, db.ForeignKey('seller.id', onupdate="CASCADE", ondelete="CASCADE"), primary_key=True)
     id_user = db.Column(db.Integer, db.ForeignKey('user.id', onupdate="CASCADE", ondelete="CASCADE"), primary_key=True)
-
+ 
     seller = db.relationship("Seller", backref="user_seller")
     user = db.relationship("User", backref="user_seller")
-
-
+ 
 class Seller(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(75), unique=True, nullable=False)
     cnpj = db.Column(db.String(20), unique=True, nullable=False)
-    seller_domain = db.Column(db.String(15), nullable=False, default="")
-
-
+ 
+ 
 class SellerVehicles(db.Model):
     id_seller = db.Column(db.Integer, db.ForeignKey(
         'seller.id', onupdate="CASCADE", ondelete="CASCADE"
@@ -149,15 +147,15 @@ class SellerVehicles(db.Model):
     vehicle_name = db.Column(db.String(255), db.ForeignKey(
         'vehicle.vehicle_name', onupdate="CASCADE", ondelete="CASCADE"
     ), primary_key=True)
-
+ 
     sellers = db.relationship(
         'Seller', backref='seller_v', lazy=True
     )
     vehicles = db.relationship(
         'Vehicle', backref='vehicle_s', lazy=True
     )
-
-
+ 
+ 
 class SellerBrands(db.Model):
     id_seller = db.Column(db.Integer, db.ForeignKey(
         'seller.id', onupdate="CASCADE", ondelete="CASCADE"
@@ -165,15 +163,15 @@ class SellerBrands(db.Model):
     hash_brand = db.Column(db.String(255), db.ForeignKey(
         'vehicle_brand.hash_brand', onupdate="CASCADE", ondelete="CASCADE"
     ), primary_key=True)
-
+ 
     sellers = db.relationship(
         'Seller', backref='seller_b', lazy=True
     )
     vehicle_brand = db.relationship(
         'VehicleBrand', backref='brands_s', lazy=True
     )
-
-
+ 
+ 
 class SellerCategories(db.Model):
     id_seller = db.Column(db.Integer, db.ForeignKey(
         'seller.id', onupdate="CASCADE", ondelete="CASCADE"
@@ -181,21 +179,21 @@ class SellerCategories(db.Model):
     hash_category = db.Column(db.String(255), db.ForeignKey(
         'category.hash_category', onupdate="CASCADE", ondelete="CASCADE"
     ), primary_key=True)
-
+ 
     sellers = db.relationship(
         'Seller', backref='seller_c', lazy=True
     )
     categories = db.relationship(
         'Category', backref='category_s', lazy=True
     )
-
-
+ 
+ 
 class Label(db.Model):
     name = db.Column(db.String(255), primary_key=True)
     id_seller = db.Column(db.Integer, db.ForeignKey(
         'seller.id', onupdate="CASCADE", ondelete="CASCADE"
     ), primary_key=True)
-
+ 
  
 class CustomShowcase(db.Model):
     cod_product = db.Column(db.String(255), db.ForeignKey(
--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -7,29 +7,29 @@ from app.extensions import db
 from app.middleware.api_token import require_api_key
 from app.models import User, User_seller
 from app.utils.functions import serialize_meta_pagination, serialize_users
-
-
+ 
+ 
 auth_bp = Blueprint("authentication", __name__)
-
-
+ 
+ 
 @auth_bp.route("/register/<string:id_seller>", methods=["POST"])
 def register(id_seller):
     data = request.get_json()
-
+ 
     username = data.get("username")
     email = data.get("email")
     password = data.get("password")
     cnpj = data.get("cnpj")
     whatsapp = data.get("whatsapp")
-
+ 
     if not username or not email or not password:
         return jsonify({"message": "Todos os campos são obrigatórios"}), 400
-
+ 
     existing_user = User.query.filter_by(email=email).first()
-
+ 
     if existing_user:
-        return jsonify({"message": "O email já está em uso"}), 400
-
+        return jsonify({"message": "O email já está em uso", "user": existing_user.serialize()}), 400
+ 
     # Hash the user password before storing
     new_user = User(
         email=email,
@@ -38,92 +38,92 @@ def register(id_seller):
         cnpj=cnpj,
         whatsapp=whatsapp
     )
-    
+   
     db.session.add(new_user)
     db.session.flush()
-    
+   
     user_seller = User_seller(
         id_seller=id_seller,
         id_user=new_user.id
     )
-
+ 
     db.session.add(user_seller)
     db.session.commit()
-
+ 
     return jsonify({"message": "Usuário registrado com sucesso!"}), 201
-
-
+ 
+ 
 @auth_bp.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-
+ 
     email = data.get("email")
     password = data.get("password")
-
+ 
     user = User.query.filter_by(email=email).first()
-
+ 
     if not user or not check_password_hash(user.password, password):
         return jsonify({"message": "Credenciais inválidas"}), 401
-
+ 
     # Create a enw JWT token with the user's id as identity
     access_token = create_access_token(identity=str(user.id))
-
+ 
     logged_user = {
         "username": user.username,
         "email": user.email
     }
-
+ 
     return jsonify({
         "message": "Login bem sucediddo",
         "access_token": access_token,
         "user": logged_user
     }), 200
-
-
+ 
+ 
 @auth_bp.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
     current_user_id = get_jwt_identity()
     user = User.query.get(current_user_id)
-
+ 
     return jsonify({
         "message": f"Bem vindo {user.username}",
     })
-    
-    
+   
+   
 @auth_bp.route("/get-seller-users/<string:id_seller>", methods=["GET"])
 @require_api_key
 def get_seller_users(id_seller):    
     page = request.args.get("page", 1, type=int)
     per_page = request.args.get("per_page", 16, type=int)
-    
+   
     if not id_seller:
         return jsonify({"message": "Query parameter 'id_seller' (integer) is required."}), 400
-    
+   
     query = (
         db.session.query(User)
             .join(User_seller, User_seller.id_user == User.id)
             .filter(User_seller.id_seller == id_seller)
     )
-    
+   
     total = db.session.query(func.count()).select_from(
         query.subquery()
     ).scalar()
-    
+   
     offset = (page - 1) * per_page
     user_rows = query.offset(offset).limit(per_page).all()
-    
+   
     items = serialize_users(user_rows)
     total_pages = ceil(total / per_page) if total else 0
-    
+   
     meta = serialize_meta_pagination(
         total=total,
         pages=total_pages,
         page=page,
         per_page=per_page
     )
-    
+   
     return jsonify({
         "meta": meta,
         "users": items
-    })
+    }), 200
